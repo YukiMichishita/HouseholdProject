486153004ac30b05db771d6a40d0acedd45c59ec
道下祐樹 <yuki@michishitayuukinoMacBook-Air.local>
2021-02-27 11:27:24 +0900

    create HouseholdProject

---
+++ Householdapp/forms.py
@@ -0,0 +1,22 @@
+from django import forms
+from .models import Refrigerator, Grocery, Unit
+
+
+class RefrigeratorForm(forms.Form):
+    grocery_names = [(g.id, g.name) for g in Grocery.objects.all()]
+    unit_names = [(u.id, u.name) for u in Unit.objects.all()]
+    grocery = forms.ChoiceField(choices=grocery_names, label='種類')
+    quantity = forms.IntegerField(label='数量')
+    unit = forms.ChoiceField(choices=unit_names, label='単位')
+
+    # def save(self):
+    #     id = self.cleaned_data.get('input_name')
+    #     selected_grocery = Grocery.objects.get(pk=id)
+
+
+class UnitForm(forms.Form):
+    unit_name = forms.CharField(label='名称')
+
+
+class GroceryForm(forms.Form):
+    grocery_name = forms.CharField(label='名称')
---
+++ Householdapp/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from .views import Refrigeratorview
+from .views import RefrigeratorAddview
+from .views import GroceryAddview
+from .views import UnitAddview
+from .forms import RefrigeratorForm
+
+
+app_name = 'Householdapp'
+urlpatterns = [
+    path('refrigerator/', Refrigeratorview, name='refrigerator'),
+    path('refrigeratorAdd/', RefrigeratorAddview, name='refrigeratorAdd'),
+    path('groceryAdd/', GroceryAddview, name='groceryAdd'),
+    path('unitAdd/', UnitAddview, name='UnitAdd'),
+    path('', RefrigeratorForm, name='RefrigeratorForm'),
+]
