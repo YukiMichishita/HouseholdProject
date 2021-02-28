46b272b954ce4b4fe950bec905c7a369dc4a2cdf
Debian <bitnami@ip-172-26-14-145.ap-northeast-1.compute.internal>
2021-02-28 07:07:20 +0000

    remove all

--- Householdapp/admin.py
+++ Householdapp/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Grocery, Refrigerator, Category, Housekeeping_Book
-
-admin.site.register(Grocery)
-admin.site.register(Refrigerator)
-admin.site.register(Category)
-admin.site.register(Housekeeping_Book)
+#from .models import Grocery, Refrigerator, Category, Housekeeping_Book
+#
+#admin.site.register(Grocery)
+#admin.site.register(Refrigerator)
+#admin.site.register(Category)
+#admin.site.register(Housekeeping_Book)
--- Householdapp/models.py
+++ Householdapp/models.py
@@ -3,27 +3,27 @@ from django.db import models
 # Create your models here.
 
 
-class Grocery(models.Model):
-    name = models.CharField(max_length=100)
-
-
-class Unit(models.Model):
-    name = models.CharField(max_length=50)
-
-
-class Refrigerator(models.Model):
-    grocery = models.ForeignKey('Grocery', on_delete=models.CASCADE)
-    unit = models.ForeignKey('Unit', on_delete=models.PROTECT)
-    quantity = models.IntegerField(max_length=10)
-    purchase_date = models.DateField(auto_now_add=True)
-
-
-class Category(models.Model):
-    category_name = models.CharField(max_length=50)
-
-
-class Housekeeping_Book(models.Model):
-    category = models.ForeignKey('Category', on_delete=models.CASCADE)
-    price = models.IntegerField(max_length=10)
-    quantity = models.IntegerField(max_length=10)
-    purchase_date = models.DateField(auto_now_add=True)
+#class Grocery(models.Model):
+#    name = models.CharField(max_length=100)
+#
+#
+#class Unit(models.Model):
+#    name = models.CharField(max_length=50)
+#
+#
+#class Refrigerator(models.Model):
+#    grocery = models.ForeignKey('Grocery', on_delete=models.CASCADE)
+#    unit = models.ForeignKey('Unit', on_delete=models.PROTECT)
+#    quantity = models.IntegerField(max_length=10)
+#    purchase_date = models.DateField(auto_now_add=True)
+#
+#
+#class Category(models.Model):
+#    category_name = models.CharField(max_length=50)
+#
+#
+#class Housekeeping_Book(models.Model):
+#    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+#    price = models.IntegerField(max_length=10)
+#    quantity = models.IntegerField(max_length=10)
+#    purchase_date = models.DateField(auto_now_add=True)
